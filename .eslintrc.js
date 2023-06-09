7cdb3bbb42eb15162c92d54bff858980f8ddbe1a
Joshua Daniel Pratt Nielsen <jdpnielsen@users.noreply.github.com>
2023-06-09 09:50:49 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.lint.json'],
+	},
+	'env': {
+		'node': true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	rules: {
+		quotes: ['warn', 'single', { avoidEscape: true }],
+		'@typescript-eslint/no-use-before-define': ['warn', { functions: false }],
+		'@typescript-eslint/indent': [
+			'error',
+			'tab',
+			{
+				'ignoredNodes': [
+					'PropertyDefinition[decorators]',
+					'TSUnionType',
+					'TSTypeParameterInstantiation',
+					'TSIntersectionType'
+				],
+				'SwitchCase': 1
+			}
+		],
+	},
+};
