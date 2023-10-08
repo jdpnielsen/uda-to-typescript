8336366dbeb5c7424e63493aff7eb86f337d3613
Joshua Daniel Pratt Nielsen <jdpnielsen@gmail.com>
2023-10-08 20:37:23 +0200

    test: add unit test for buildTypes

--- src/lib/build-types.spec.ts
+++ src/lib/build-types.spec.ts
@@ -1,11 +1,36 @@
 import { collectArtifacts } from './helpers/collect-artifacts';
 import { buildTypes } from './build-types';
+import ts from 'typescript';
+import { readFile } from 'fs/promises';
 
 describe('buildTypes', () => {
-	it('Should handle a glob', async () => {
-		const artifacts = await collectArtifacts('./files/*.uda');
+	it('Should handle a glob with no files', async () => {
+		const artifacts = await collectArtifacts('./__bad_path/*.uda');
 
 		const output = buildTypes(artifacts);
+		const expected = ts.createPrinter()
+			.printList(
+				ts.ListFormat.SingleLine,
+				output,
+				ts.createSourceFile('', '', ts.ScriptTarget.Latest)
+			);
 
+		expect(expected.trim())
+			.toBe('import { type BaseDocumentType, type EmptyObjectType, type BaseGridBlockType, type BaseBlockType, type BaseGridBlockAreaType, type BaseBlockListType, type BaseBlockGridType, type BaseMediaType, type Crop, type MediaPickerItem } from "./base-types";');
+	});
+
+	it('Should handle a glob with files', async () => {
+		const artifacts = await collectArtifacts('./src/__tests__/__fixtures__/*.uda');
+		const expected = await readFile('./src/__tests__/__fixtures__/output.txt');
+
+		const output = buildTypes(artifacts);
+		const actual = ts.createPrinter()
+			.printList(
+				ts.ListFormat.MultiLine,
+				output,
+				ts.createSourceFile('', '', ts.ScriptTarget.Latest)
+			);
+
+		expect(actual).toBe(expected.toString());
 	});
 });
