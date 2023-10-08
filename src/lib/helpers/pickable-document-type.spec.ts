b7c439342f373657844c1baa692fe18c0f0509b3
Joshua Daniel Pratt Nielsen <jdpnielsen@gmail.com>
2023-10-08 15:28:24 +0200

    test: fix broken getPickableTypes test

--- src/lib/helpers/pickable-document-type.spec.ts
+++ src/lib/helpers/pickable-document-type.spec.ts
@@ -1,25 +1,28 @@
 import { collectArtifacts } from './collect-artifacts';
-import { getPickableDocumentTypes } from './pickable-document-type';
+import { getPickableTypes } from './pickable-document-type';
 
-describe('getPickableDocumentTypes', () => {
+describe('getPickableTypes', () => {
 	it('Should count root document as pickable', async () => {
 		const artifacts = await collectArtifacts('./src/__tests__/__fixtures__/document-type__3a0918f517e14625922203299b168d11.uda');
+		const documentTypes = Array.from(artifacts['document-type'].values());
 
-		const dataTypes = getPickableDocumentTypes(artifacts);
-		expect(dataTypes).toHaveLength(1);
+		const pickable = getPickableTypes(documentTypes);
+		expect(pickable).toHaveLength(1);
 	});
 
 	it('Should not count non-root document as pickable', async () => {
 		const artifacts = await collectArtifacts('./src/__tests__/__fixtures__/document-type__1ab5ade5519e42709b4cc554968d97c9.uda');
+		const documentTypes = Array.from(artifacts['document-type'].values());
 
-		const dataTypes = getPickableDocumentTypes(artifacts);
-		expect(dataTypes).toHaveLength(0);
+		const pickable = getPickableTypes(documentTypes);
+		expect(pickable).toHaveLength(0);
 	});
 
 	it('Should count documents referenced via AllowedChildContent as pickable', async () => {
 		const artifacts = await collectArtifacts('./src/__tests__/__fixtures__/*.uda');
+		const documentTypes = Array.from(artifacts['document-type'].values());
 
-		const dataTypes = getPickableDocumentTypes(artifacts);
-		expect(dataTypes).toHaveLength(2);
+		const pickable = getPickableTypes(documentTypes);
+		expect(pickable).toHaveLength(2);
 	});
 });
