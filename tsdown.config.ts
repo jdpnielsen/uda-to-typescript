f28880e9edc355c4ee912fd5297dc304d1608943
Joshua Nielsen <jdpnielsen@gmail.com>
2026-02-22 22:55:52 +0100

    chore: run publint and attw

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,5 +3,20 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
 	exports: true,
 	entry: ['./src/index.ts', './src/cli.ts'],
-	format: ['esm', 'cjs'],
+	format: {
+		esm: {
+			target: ['esnext'],
+		},
+		cjs: {
+			target: ['node20'],
+		},
+	},
+	dts: {
+		sourcemap: true,
+	},
+	report: true,
+	publint: true,
+	attw: {
+		profile: 'node16',
+	},
 });
